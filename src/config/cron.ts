6dd478094ec3c8bb6dcc62944a4cc6d37d9ef698
Deepali mahajan <deepalisatore@gmail.com>
2026-04-06 22:52:13 +0530

    anew channges

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -2,10 +2,11 @@
 import cron from "node-cron";
 import axios from "axios";
 
-const SERVER_URL = "https://your-render-url.onrender.com/keep-alive";
+const SERVER_URL = "https://demo-versale.onrender.com/keep-alive";
 
 export const startCron = () => {
-  cron.schedule("*/40 * * * *", async () => {
+  // Every 40 seconds
+  cron.schedule("*/40 * * * * *", async () => {
     try {
       const res = await axios.get(SERVER_URL);
       console.log("✅ Ping Success:", res.data);
